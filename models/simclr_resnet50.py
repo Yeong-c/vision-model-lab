0011fe8cc91147e0c4cc7717be3e17956a3f8834
syungkyu03 <syungkyu03@naver.com>
2025-12-22 13:41:23 +0900

    Update simclr comment

--- models/simclr_resnet50.py
+++ models/simclr_resnet50.py
@@ -15,11 +15,12 @@ class NTXentLoss(nn.Module):
         # z [2*Batch_Size, Projection Dim]
         N = z.shape[0] // 2
 
-        # Con Sim을 위해 Normalize
+        # Cos Sim을 위해 Normalize
         z = nn.functional.normalize(z, dim=1)
 
+        # SimCLR 유명 구현체들 참고, 행렬곱으로 이중 for문 대체
+        # 대각 성분 -10억으로(자기 자신)
         sim_matrix = torch.matmul(z, z.T) / self.temperature
-
         mask = torch.eye(2 * N, device=z.device).bool()
         sim_matrix.masked_fill_(mask, -1e9)
 
@@ -31,7 +32,7 @@ class NTXentLoss(nn.Module):
         loss = self.criterion(sim_matrix, target)
         return loss / (2 * N)
 
-
+# 기존에 만들어 둔 ResNet50 이용
 class SimCLR_ResNet50(ResNet50):
     def __init__(self, input_shape=(3, 32, 32), projection_dim=128):
         super(SimCLR_ResNet50, self).__init__(input_shape, num_classes=10)
