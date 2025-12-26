80e8becd28b7f1baddcd6836f5ccd4883d843ebd
syungkyu03 <syungkyu03@naver.com>
2025-12-27 04:12:16 +0900

    main.py Train, Test 수정

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import argparse
+import os
 import torch
 import models, methods, dataset, optims # 우리 것들
-import tqdm
+import tqdm, math
 
 #rotnet 평가용
 from methods.rotnet import rotate_batch
@@ -124,8 +125,9 @@ def train_one_epoch(args, dataloader, model, optimizer, scheduler, device, epoch
         total_loss += loss.item()
 
         # 10 배치마다 로그 출력
-        if (batch_idx + 1) % 10 == 0:
-            print(f"  [Batch {batch_idx+1}/{len(dataloader)}] Loss: {loss.item():.4f}")
+        if (batch_idx + 1) % 5 == 0:
+            #print(f"\n[Batch {batch_idx+1}/{len(dataloader)}] Loss: {loss.item():.4f}")
+            pass
 
     # return 에포크 평균 Loss
     return total_loss / len(dataloader)
@@ -140,22 +142,64 @@ def train_model(args, test_loader, train_loader, model, optimizer, scheduler, de
     print(f"Optimizer: {args.optimizer}\tScheduler: {args.scheduler}\t\tLR: {args.lr}")
     print("="*50)
 
+    # 한 Epoch 학습 실행
     for ep in range(args.epoch):
-        train_one_epoch(args, train_loader, model, optimizer, scheduler, device, epoch_cnt=ep+1)
-        test_model_accuracy(args, test_loader, model, device)
+        # 1 Epoch 학습 후 Train Loss 출력
+        train_loss = train_one_epoch(args, train_loader, model, optimizer, scheduler, device, epoch_cnt=ep+1)
+        print(f"   [Epoch {ep+1}] Train Loss: {train_loss:.4f}")
+
+
+        # Test(Eval) 진행
+        if args.method == "supervised":
+            # supervised면 epoch마다 Accuracy 측정 후 출력
+            acc = test_model_accuracy(args, test_loader, model, device)
+            print(f"   [Epoch {ep+1}] Accuracy: {acc:.2f}%")
+
+        elif args.method == "rotnet":
+            # rotnet이면 Rotation Accuracy랑 Feature Accuracy(KNN) 전부 측정
+            # 대신 KNN은 5 Epoch마다
+
+            # Rotation Acc
+            acc = test_model_accuracy(args, test_loader, model, device)
+            print(f"   [Epoch {ep+1}] Rotation Accuracy: {acc:.2f}%")
+
+            # Feature Acc(KNN)
+            if (ep + 1) % 5 == 0:
+                print("   **Calculating KNN Accuracy**")
+                acc = test_model_KNN(args, train_loader, test_loader, model, device, 200)
+                print(f"   [Epoch {ep+1}] KNN Accuracy: {acc:.2f}%")
+
+        else:
+            # 아니면 Epoch 5개마다 KNN으로 측정
+            if (ep + 1) % 5 == 0:
+                if args.method == "simclr":
+                    print("   **Calculating KNN Accuracy**")
+                    acc = test_model_KNN(args, train_loader, test_loader, model, device, 200)
+                    print(f"   [Epoch {ep+1}] KNN Accuracy: {acc:.2f}%")
+
+        # 모델 저장(checkpoint)
+        os.makedirs("./checkpoints", exist_ok=True)
+        save_path = f"./checkpoints/{args.model}_{args.method}_{args.dataset}.pth"
+
+        # 계속 덮어쓰기
+        if args.method == "supervised":
+            torch.save(model.state_dict(), save_path)
+        else:
+            # Supervised 아닌건 Backbone State를 저장
+            torch.save(model.model.state_dict(), save_path)
 
     print("="*50)
     print("**FINISH TRAINING**")
     print("="*50)
 
-
-def test_model_accuracy(args, dataloader, model, device):
+# Accuracy Test 함수
+def test_model_accuracy(args, test_loader, model, device):
     model.eval()
     correct = 0
     total = 0
 
     with torch.no_grad():
-        for batch in dataloader:
+        for batch in test_loader:
             x, y = batch
 
             if args.method=="rotnet":
@@ -173,11 +217,63 @@ def test_model_accuracy(args, dataloader, model, device):
             correct += (predicted == y).sum().item()
             
     acc = correct / total * 100
-    print(f"Accuracy: {acc:.2f}%")
     return acc
 
-def test_model_KNN(args, dataloader, model, device):
-    pass
+# KNN Test 함수
+def test_model_KNN(args, train_loader, test_loader, model, device, K):
+    model.eval()
+    correct = 0
+    total = 0
+
+    train_features = []
+    train_labels = []
+
+    # Train Set Feature들 저장
+    with torch.no_grad():
+        for batch in train_loader:
+            x, y = batch
+            x, y = x.to(device), y.to(device)
+
+            if x.dim() == 5:
+                x = x[:, 0, :, :, :]
+            
+            # Backbone에 TrainSet 넣어서 Feature 추출 (model.model)
+            feature = model.model(x)
+            # 정규화
+            feature = torch.nn.functional.normalize(feature, dim=1)
+
+            train_features.append(feature)
+            train_labels.append(y)
+
+        train_features = torch.cat(train_features, dim=0).t()
+        train_labels = torch.cat(train_labels, dim=0)
+
+        for batch in test_loader:
+            x, y = batch
+            x, y = x.to(device), y.to(device)
+
+            # Test Feature 추출
+            test_feature = model.model(x)
+            test_feature = torch.nn.functional.normalize(test_feature, dim=1)
+
+            # test feature와 Train Feature들에 대한 거리 계산(행렬곱)
+            # (Test, Dim) X (Dim, Train) -> (Test, Train)
+            sim_matrix = torch.matmul(test_feature, train_features)
+
+            # 제일 가까운 K개 이웃 찾기 (거리, 인덱스)
+            distances, indices = sim_matrix.topk(K, dim=1, largest=True, sorted=True)
+
+            # 인덱스로 라벨 가져오기
+            neighbors = train_labels[indices] # [Batch Size, K]
+
+            # 가장 많이 나온 라벨이 뭐지?
+            knn, _ = torch.mode(neighbors, dim=1)
+
+            total += x.size(0)
+            correct += (knn == y).sum().item()
+        
+    acc = correct / total * 100
+    return acc
 
 
 
