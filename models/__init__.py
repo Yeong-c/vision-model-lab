be0d87eeaa677bbe027c1cc7982e56149dc14cf6
leehoney030 <99636093+leehoney030@users.noreply.github.com>
2026-01-01 12:22:33 +0900

    호출 통일 / vit 추가
    Updated get_model function to accept input_shape and modified model instantiation for ResNet, DenseNet, and ViT accordingly.

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,18 @@
 from .resnet import ResNet
 from .densenet import DenseNet
+from .vit import ViT
 
-def get_model(model_name):
-    model_name = model_name.lower()
+def get_model(model_name, input_shape):
+
     if model_name == "resnet18":
-        model = ResNet(num_layers=18)
+        model = ResNet(num_layers=18, input_shape=input_shape)  
     elif model_name == "resnet50":
-        model = ResNet(num_layers=50)
-    elif model_name == "vit":
-        model ="vit"
+        model = ResNet(num_layers=50, input_shape=input_shape)
     elif model_name == "densenet":
-        model = DenseNet()
+        model = DenseNet(input_shape=input_shape)
+    elif model_name == "vit":
+        model = ViT(input_shape=input_shape)
     else:
-        model = "null"
+        raise ValueError(f"null: {model_name}")
+    
     return model
